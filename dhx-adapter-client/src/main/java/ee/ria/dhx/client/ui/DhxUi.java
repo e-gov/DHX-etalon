2d564f50e65dd274889c98fedc24e7c689c837d3
aleksei-kokarev <alexksei@gmail.com>
2017-01-06 16:06:22 +0200

    logo

--- dhx-adapter-client/src/main/java/ee/ria/dhx/client/ui/DhxUi.java
+++ dhx-adapter-client/src/main/java/ee/ria/dhx/client/ui/DhxUi.java
@@ -172,7 +172,7 @@ public class DhxUi extends UI {
     mainLabel.setStyleName("h1");
     StreamResource source =
         new StreamResource(new DhxStreamSource(
-            "jar://images/EL_struktuuri-_ja_investeerimisfondid_horisontaalne.jpg"), "help.png");
+            "jar://images/EL_Regionaalarengu_Fond_horisontaalne.jpg"), "help.png");
     Image image = new Image("", source);
     image.setStyleName("v-table-footer-container");
     GridLayout headerLayout = new GridLayout(2, 2);
